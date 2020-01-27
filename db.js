78a26e181f658203acb4f96a290be8626456a0a3
Chris Douglass <revmonkey1977@gmail.com>
2020-01-26 20:50:31 -0800

    Create db.js
    - added promisified pool file

---
+++ db.js
@@ -0,0 +1,31 @@
+//Promisified MySQL query setup, adapted from: https://medium.com/@mhagemann/create-a-mysql-database-middleware-with-node-js-8-and-async-await-6984a09d49f4
+
+var mysql   =   require('mysql')
+var util    =   require('util')
+
+var pool    =   mysql.createPool({
+                    connectionLimit: 10,
+                    host: 'localhost',
+                    user: 'root',
+                    password: 'plethora of pinatas',
+                    database: 'soul_exchange_v2'
+                })
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
+    }    if (connection) connection.release()    
+    return
+})
+
+pool.query  =   util.promisify(pool.query);
+
+module.exports = pool
